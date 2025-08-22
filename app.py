b11895044bc2336ee0505a68d6b3bec86296c5d4
Dhruv kaushik <dhruvkaushik2810@gmail.com>
2025-08-23 01:16:47 +0530

    Done

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ import os
 app = Flask(__name__)
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from src.prompt import system_prompt
 
-# OpenAI model global scope me
-chatModel = ChatOpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
+chatModel = ChatGroq(model="llama-3-8b-8192", api_key=GROQ_API_KEY)
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
     ("human", "{input}"),
@@ -35,11 +34,10 @@ def about():
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
-    # Embeddings aur Pinecone index yahan lazy load karo
     from langchain_openai import OpenAIEmbeddings
     from langchain_pinecone import PineconeVectorStore
 
-    embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+    embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
     index_name = "medical-chatbot"
     docsearch = PineconeVectorStore.from_existing_index(
         index_name=index_name,
