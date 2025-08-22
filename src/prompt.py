e9b4331d89edba9c33b3e47a5f2a04deacac6e40
Dhruv kaushik <dhruvkaushik2810@gmail.com>
2025-08-23 01:58:44 +0530

    Done

--- src/prompt.py
+++ src/prompt.py
@@ -1,9 +1,5 @@
 system_prompt = (
-    "You are an Medical assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
+    "You are a helpful medical assistant. "
+    "Answer user questions concisely. "
+    "If you don't know the answer, say you don't know."
 )
