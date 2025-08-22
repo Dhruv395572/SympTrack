3d555c372d7d2ddeff1ab433566ebbc2fd8601ec
Dhruv kaushik <dhruvkaushik2810@gmail.com>
2025-08-22 22:46:22 +0530

    Done

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask, render_template, jsonify, request
-from src.helper import download_hugging_face_embeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_openai import ChatOpenAI
-from langchain_groq import ChatGroq
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from src.prompt import *
 import os
 
-
 app = Flask(__name__)
-
-
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
@@ -21,33 +10,6 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
-
-embeddings = download_hugging_face_embeddings()
-
-index_name = "medical-chatbot" 
-# Embed each chunk and upsert the embeddings into your Pinecone index.
-docsearch = PineconeVectorStore.from_existing_index(
-    index_name=index_name,
-    embedding=embeddings
-)
-
-
-
-
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
-
-chatModel = ChatGroq(model="llama-3.1-8b-instant", api_key=os.getenv("GROQ_API_KEY"))
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
-
-question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
-
-
 
 @app.route("/")
 def index():
@@ -63,14 +25,35 @@ def about():
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
+    from src.helper import download_hugging_face_embeddings
+    from langchain_pinecone import PineconeVectorStore
+    from langchain_groq import ChatGroq
+    from langchain.chains import create_retrieval_chain
+    from langchain.chains.combine_documents import create_stuff_documents_chain
+    from langchain_core.prompts import ChatPromptTemplate
+    from src.prompt import system_prompt
+
+    embeddings = download_hugging_face_embeddings()
+    index_name = "medical-chatbot"
+    docsearch = PineconeVectorStore.from_existing_index(
+        index_name=index_name,
+        embedding=embeddings
+    )
+    retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+    chatModel = ChatGroq(model="llama-3.1-8b-instant", api_key=os.getenv("GROQ_API_KEY"))
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ])
+    question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
     msg = request.form["msg"]
     input = msg
     print(input)
     response = rag_chain.invoke({"input": msg})
     print("Response : ", response["answer"])
     return str(response["answer"])
-
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
